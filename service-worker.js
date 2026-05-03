78445762fc48c9c61bb4fe2386cb61e1c1b0a6a0
NthabieMogatle <120685206+NthabieMogatle@users.noreply.github.com>
2026-05-03 02:04:50 -0400

    Updated SafeFlex interactive version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "smc-safeflex-v1";
+const CACHE_NAME = "smc-safeflex-v2";
 const FILES = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))));
+self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))); });
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))));
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
